862e4ba537961602e0fb1d9e5852e3214aaaae19
huydevct <huyvipct1@gmail.com>
2021-10-12 00:39:03 +0700

    routes and controllers and connecting the mongoDB

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRouter from './routes/post.js';
+
+const app = express();
+
+app.use('/posts', postRouter);
+
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://huyctdev:huyctdev123@cluster0.zfyia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err))
+
