e1a6373888aa70181b240a8a70d4428bb331abb0
huydevct <huyvipct1@gmail.com>
2021-10-13 16:52:06 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 import postRouter from './routes/post.js';
 
 const app = express();
+dotenv.config()
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -12,10 +14,14 @@ app.use(cors());
 
 app.use('/posts', postRouter);
 
+app.get('/', (req,res) => {
+    res.send("Hello to MERN API")
+})
+
 const CONNECTION_URL = 'mongodb+srv://huyctdev:huyctdev123@cluster0.zfyia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err))
 
